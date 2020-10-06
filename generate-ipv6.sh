d613d04ee982107876f1919586daa3a9f81ccb75
rimu <rimu@rimu.geek.nz>
2020-10-06 19:53:04 +1300

    more nazis

--- generate-ipv6.sh
+++ generate-ipv6.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#running this script will regenerate the ipv6 versions of the blocklist based on the contents of the IPv4 blocklist in the .txt files
 
 cp dnsmasq_hosts.txt dnsmasq_hosts.txt.ipv6
 sed -i 's/0.0.0.0 /::1 /g' dnsmasq_hosts.txt.ipv6
