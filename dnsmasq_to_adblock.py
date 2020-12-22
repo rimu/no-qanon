4d50ad84b3510f4ad3ea46f156cced6bae0a35b0
rimu <rimu@rimu.geek.nz>
2020-12-23 08:02:24 +1300

    adblock generation scripts

---
+++ dnsmasq_to_adblock.py
@@ -0,0 +1,12 @@
+# this script converts dnsmasq_hosts.txt into a format used by the adblock extension
+# usage:
+#	python dnsmasq_to_adblock.py > adblock.txt
+
+text_file = open("dnsmasq_hosts.txt", "r")
+lines = text_file.readlines()
+text_file.close()
+
+for line in lines:
+	parts = line.split(' ')
+	if len(parts) > 1:
+		print '||' + parts[1].strip() + '^'
