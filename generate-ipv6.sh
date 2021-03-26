193f4b1cdde98c6cf6a7567ccda7a8ed11b5d090
Rimu Atkinson <rimuatkinson@gmail.com>
2021-03-27 11:30:49 +1300

    make ipv6 generation script compatible with mac

--- generate-ipv6.sh
+++ generate-ipv6.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 #running this script will regenerate the ipv6 versions of the blocklist based on the contents of the IPv4 blocklist in the .txt files
 
+SED=sed
+unamestr=`uname`
+if [[ "$unamestr" == "Darwin" ]] ; then
+    SED=gsed
+    type $SED >/dev/null 2>&1 || {
+        echo >&2 "$SED it's not installed. Try: brew install gnu-sed" ;
+        exit 1;
+    }
+fi
+
 cp dnsmasq_hosts.txt dnsmasq_hosts.txt.ipv6
-sed -i 's/0.0.0.0 /::1 /g' dnsmasq_hosts.txt.ipv6
+$SED -i 's/0.0.0.0 /::1 /g' dnsmasq_hosts.txt.ipv6
 
 cp etc_hosts.txt etc_hosts.txt.ipv6
-sed -i 's/0.0.0.0 /::1 /g' etc_hosts.txt.ipv6
+$SED -i 's/0.0.0.0 /::1 /g' etc_hosts.txt.ipv6
 
